c928a6be89f7d230962bbaedaece5d873dd83907
Teemu <consolelogger@ftml.net>
2025-02-23 03:14:54 +0200

    Course Information, step 1 done

---
+++ part_1/1_courseinfo_step1/src/App.jsx
@@ -0,0 +1,47 @@
+
+
+const Header = (props) => {   //props from App component passed to Header component
+  return (
+  <h1>{props.course}</h1>
+)
+}
+
+const Content = (props) => {   //props from App component passed to Content component
+   return (
+    <div>
+    <p> {props.part1} {props.exercises1} </p>
+    <p> {props.part2} {props.exercises2} </p>
+    <p> {props.part3} {props.exercises3} </p>
+    </div>
+  )
+}
+
+const Total = (props) => {   //props from App component passed to Total component
+  return (
+    <p> {props.exercises}</p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+    <Header course={course}/>
+    <Content part1={part1+exercises1}/>
+    <Content part2={part2+exercises2}/>
+    <Content part3={part3+exercises3}/>
+    <Total exercises={ (exercises1 + exercises2 + exercises3) }/>
+    </div>
+  )
+  // const course passed to Header as props named course
+}
+
+export default App
