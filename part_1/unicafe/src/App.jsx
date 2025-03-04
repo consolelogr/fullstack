8a63547e8d5b01463f8fb5d2d4d565f4bedca4b7
Teemu <consolelogger@ftml.net>
2025-03-04 16:03:09 +0200

    unicafe step 2

---
+++ part_1/unicafe/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const average = (good - bad) / (good + neutral + bad);
+  const positive =  100* (good) / (neutral + bad);
+ 
+
+    return (
+    <div className="feedback">
+      <h1>Unicafe</h1>
+      <p>Give feedback</p>
+    
+      <button onClick= { () => 
+        {const goodCount=good+1; setGood(goodCount)}  }> Good </button> 
+        
+      <button onClick= { () => 
+        { const neutralCount=neutral+1; setNeutral(neutralCount);  }}> Neutral </button>
+        
+      <button onClick= { () => 
+        { const badCount=bad+1 ;setBad(badCount);  }}> Bad </button>
+        
+        <br/><br/>
+        <h2>Statistics</h2>
+      <p>Good: {good} <br/> Neutral: {neutral} <br/> Bad: {bad}</p>
+
+      <p>Average: {isNaN(average) ? '0.0' : average.toFixed(1)}</p>
+      <p>Positive: {isNaN(positive) ? '0.0' : positive.toFixed(1)+ "%"}</p>
+  
+        <p>Total: {good + neutral + bad}</p>
+
+     
+      </div>
+    
+  )
+}
+
+export default App
