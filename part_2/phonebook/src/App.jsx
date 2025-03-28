7945284dd888eee537c5d856bbca538c5c897a1d
Teemu <consolelogger@ftml.net>
2025-03-28 16:26:59 +0200

    Phonebook step4

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'  //named import
 
 const App = () => { //block body {}
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: "040-1234567" }])  /*state initialization. React useState hook returns an array, and we use array destructuring to pull out the values.*/
-  const [newName, setNewName] = useState('') 
-  const [newPhone, setNewPhone] = useState('') 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])  /*state initialization. React useState hook returns an array, and we use array destructuring to pull out the values.*/
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
   const [errorMessage, setErrorMessage] = useState(''); // State initialization for error message
 
   const handleSubmit = (event) => { // event handler (addName is triggered when the user submits the form)
@@ -17,9 +22,9 @@ const App = () => { //block body {}
 
     // Check for duplicate name
     if (persons.some(person => person.name === newName)) {
-     
+
       setErrorMessage(`${newName} is already added to the phonebook`);
-       return; 
+      return;
     }
 
     // Add the new name to the list
@@ -32,53 +37,81 @@ const App = () => { //block body {}
     setNewPhone('')
   }
 
+  const [search, setSearch] = useState('');
 
-  
 
   const handleNameChange = (event) => { //event (or e) handler. handleNameChange is triggered when the user types something in the input field.
-    setNewName(event.target.value) 
+    setNewName(event.target.value)
   }
   const handlePhoneChange = (event) => { //event (or e) handler. handleNameChange is triggered when the user types something in the input field.
-    setNewPhone(event.target.value) 
+    setNewPhone(event.target.value)
   }
-
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="container">
+
       <h2>Phonebook</h2>
-      <form onSubmit= {handleSubmit}>
+      <input
+        placeholder="filter shown with"
+        value={search}
+        onChange={handleSearchChange}
+      /><br />
+      <button type="text">Filter</button>
+      <br /><br />
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
-          <input 
-            placeholder="New name" 
-            value={newName} 
-            onChange={handleNameChange} 
+          <input
+            placeholder="New name"
+            value={newName}
+            onChange={handleNameChange}
           />
 
-<input 
-            placeholder="New phonenumber" 
-            value={newPhone} 
-            onChange={handlePhoneChange} 
+          <input
+            placeholder="New phonenumber"
+            value={newPhone}
+            onChange={handlePhoneChange}
           />
 
-        <br />
+          <br />
           <button type="submit">Add</button>
         </div>
       </form>
       {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-       
+      <br />
+
+      <h2>Filtered</h2>
+      {search === '' ? (   //ternary operator: condition ? doThisIfTrue : doThisIfFalse
+        <p>None</p>
+      ) : (
+        filteredPersons.length > 0 ? ( //.lenght array property
+          filteredPersons.map((person, index) => ( //creates a new array by applying a function to each element of the original array.
+            <li key={index}>{person.name}</li>
+          ))
+        ) : (
+          <p>No matches found</p>
+        )
+      )}
       <h2>Numbers</h2>
+
+
       <ul>
         {persons.map((person, index) => (
           <div>
-                      <li key={index}>{person.name}: {person.number}</li>
-          
+            <li key={index}>{person.name}: {person.number}</li>
+
           </div>
-       
-       ))}
+
+        ))}
       </ul>
 
       <br /><br />
-      
+
     </div>
   )
 }
