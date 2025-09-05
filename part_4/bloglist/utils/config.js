efb6737ffcbc0aa3c51ccebfaa7651172aba6236
Teemu <consolelogger@ftml.net>
2025-09-05 19:07:23 +0300

    4.8

--- part_4/bloglist/utils/config.js
+++ part_4/bloglist/utils/config.js
@@ -1,7 +1,11 @@
 
-const PORT = 'mongodb://localhost/bloglist'
-const MONGODB_URI = process.env.MONGODB_URI
+//const PORT = 'mongodb://localhost/bloglist'
+const PORT = process.env.PORT
 const serverPORT = 3003
+
+const MONGODB_URI = process.env.NODE_ENV === 'test' 
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
