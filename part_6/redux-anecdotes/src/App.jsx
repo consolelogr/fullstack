94a74b799c16ce95f34aec440a9bf8576b41352e
Consolelogger <consolelogger@ftml.net>
2025-12-11 19:18:34 +0200

    6.4 done

---
+++ part_6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,48 @@
+import { useSelector, useDispatch } from "react-redux";
+import "./style.css";
+
+const App = () => {
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    dispatch({ type: "VOTE", id });
+    console.log("vote", id);
+  };
+  const addAnecdote = (event) => {
+    // --> new function
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    const newAnecdote = {
+      id: Math.floor(Math.random() * 1000000), // simple unique id
+      content,
+      votes: 0,
+    };
+    dispatch({ type: "CREATE", anecdote: newAnecdote });
+    event.target.anecdote.value = ""; // clear input
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
