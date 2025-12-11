2be767783389791a40aa5423049d937a62781253
Consolelogger <consolelogger@ftml.net>
2025-12-12 00:27:23 +0200

    6.8 done

--- part_6/redux-anecdotes/src/App.jsx
+++ part_6/redux-anecdotes/src/App.jsx
@@ -1,34 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
 import "./style.css";
-import { voteAnecdote, createAnecdote } from "./reducers/anecdoteReducer";
-import AnecdoteForm from "./components/anecdoteForm";
+import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  /*const vote = (id) => {
-    dispatch({ type: "VOTE", id });
-    console.log("vote", id);
-    };*/
-  const vote = (id) => {
-    dispatch(voteAnecdote(id)); // Action creator
-  };
-
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes);
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {sortedAnecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   );
