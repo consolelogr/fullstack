4e27043977d0656fe12bd98dc601b401e1a8fb10
Consolelogger <consolelogger@ftml.net>
2025-12-16 00:04:50 +0200

    7.7 done

---
+++ part_7/country-hook/src/hooks/useCountry.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null)
+
+  useEffect(() => {
+    if (!name) {
+      setCountry(null)
+      return
+    }
+
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then(response => {
+        setCountry({ found: true, data: response.data })
+      })
+      .catch(() => {
+        setCountry({ found: false })
+      })
+  }, [name])
+
+  return country
+}
+
+export default useCountry
